84aef2d19a3bb2b3a90fdec622bd5a7d8037539b
Luciano <luciano.rodriguez59@gmail.com>
2023-11-20 17:40:16 -0300

    modified CustomError

--- src/presentation/error/CustomError.ts
+++ src/presentation/error/CustomError.ts
@@ -3,23 +3,23 @@ export class CustomError extends Error {
     super(message)
   }
 
-  static badRequest(message: string = 'Bad Request'): void {
+  static badRequest(message: string = 'Bad Request'): never {
     throw new CustomError(message, 400)
   }
 
-  static unauthorized(message: string = 'Unauthorized'): void {
+  static unauthorized(message: string = 'Unauthorized'): never {
     throw new CustomError(message, 401)
   }
 
-  static forbidden(message: string = 'Forbidden'): void {
+  static forbidden(message: string = 'Forbidden'): never {
     throw new CustomError(message, 403)
   }
 
-  static notFound(message: string = 'Not found'): void {
+  static notFound(message: string = 'Not found'): never {
     throw new CustomError(message, 404)
   }
 
-  static internalServer(message: string = 'Internal Server Error'): void {
+  static internalServer(message: string = 'Internal Server Error'): never {
     throw new CustomError(message, 500)
   }
 }
