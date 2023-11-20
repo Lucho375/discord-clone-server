c7a7e851a7c7b7ac1f3051fe726acd27f6649060
Luciano <luciano.rodriguez59@gmail.com>
2023-11-20 20:22:55 -0300

    add user.datasource abstract class

---
+++ src/domain/datasources/user.datasource.ts
@@ -0,0 +1,14 @@
+import { type UserEntity } from 'domain/entities/UserEntity.js'
+import { type IRegisterUserDto } from '../dtos/RegisterUserDto.js'
+
+export abstract class UserDatasource {
+  abstract createOne(registerUser: IRegisterUserDto): Promise<UserEntity>
+
+  abstract deleteOne(id: string): Promise<UserEntity | null>
+
+  abstract getAll(): Promise<UserEntity[]>
+
+  abstract getOne(id: string): Promise<UserEntity | null>
+
+  abstract updateOne(id: string, update: Partial<IRegisterUserDto | null>): Promise<UserEntity | null>
+}
