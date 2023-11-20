d23450ccce2103b8be099660344881c2769da08b
Luciano <luciano.rodriguez59@gmail.com>
2023-11-20 17:42:32 -0300

    add loginUserDto and registerUserDto with zod

---
+++ src/domain/dtos/LoginUserDto.ts
@@ -0,0 +1,38 @@
+import z from 'zod'
+
+export interface ILoginUserDto {
+  email: string
+  password: string
+}
+
+/**
+ * Represents a Data Transfer Object (DTO) for user authentication.
+ * This class is responsible for validating and creating instances of user authentication data.
+ */
+export class LoginUserDto {
+  public email: string
+  public password: string
+
+  private constructor({ email, password }: ILoginUserDto) {
+    this.email = email
+    this.password = password
+  }
+
+  /**
+   * Creates an instance of LoginUserDto by validating the input data.
+   * @param {ILoginUserDto} loginUserDto - User registration data to be validated and used for instantiation.
+   * @returns {ILoginUserDto} - Instance of LoginUserDto.
+   * @throws {ZodError} - Throws an error if the input data is invalid.
+   */
+
+  static create(loginUserDto: ILoginUserDto): LoginUserDto {
+    const validation = z
+      .object({
+        email: z.string().email(),
+        password: z.string().min(8)
+      })
+      .parse(loginUserDto)
+
+    return new LoginUserDto(validation)
+  }
+}
---
+++ src/domain/dtos/RegisterUserDto.ts
@@ -0,0 +1,50 @@
+import z from 'zod'
+
+export interface IRegisterUserDto {
+  dateBirth: Date
+  displayName: string
+  email: string
+  password: string
+  userName: string
+}
+
+/**
+ * Represents a Data Transfer Object (DTO) for user registration.
+ * This class is responsible for validating and creating instances of user registration data.
+ */
+export class RegisterUserDto {
+  public dateBirth: Date
+  public displayName: string
+  public email: string
+  public password: string
+  public userName: string
+
+  private constructor({ dateBirth, displayName, email, password, userName }: IRegisterUserDto) {
+    this.dateBirth = dateBirth
+    this.displayName = displayName
+    this.email = email
+    this.password = password
+    this.userName = userName
+  }
+
+  /**
+   * Creates an instance of RegisterUserDto by validating the input data.
+   * @param {IRegisterUserDto} registerUserData - User registration data to be validated and used for instantiation.
+   * @returns {RegisterUserDto} - Instance of RegisterUserDto.
+   * @throws {ZodError} - Throws an error if the input data is invalid.
+   */
+
+  static create(registerUserData: IRegisterUserDto): RegisterUserDto {
+    const validated = z
+      .object({
+        dateBirth: z.date(),
+        displayName: z.string(),
+        email: z.string().email(),
+        password: z.string(),
+        userName: z.string()
+      })
+      .parse(registerUserData)
+
+    return new RegisterUserDto(validated)
+  }
+}
